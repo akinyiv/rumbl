99304f6cee8de7abdcb6f191e75df893c55d2274
akinyiv <akinyivee10@gmail.com>
2023-05-24 08:44:42 +0300

    feature project, controllers, views and templates

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,38 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+  alias Rumbl.Repo
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    [
+      %User{id: "1", name: "José", username: "josevalim"},
+      %User{id: "2", name: "Bruce", username: "redrapids"},
+      %User{id: "3", name: "Chris", username: "chrismccord"}
+    ]
+    Repo.all(User)
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |>User.changeset(attrs)
+    |>Repo.insert()
+  end
+end
